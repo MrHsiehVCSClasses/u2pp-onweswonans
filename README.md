8085c8aa4774189f260be8d249b50e90b08f001f
Owen Swanson <owsw333@vcs.email>
2022-10-06 11:08:00 -0700

    i forgot these iones

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Empty Project Template
 Fill in this README while working on your project. 
+
+
+
+this project is to test my ability to make classes. the program starts simply by pressing run. run deez nuts. got em.
+
+Character super class stores variable and acts as a narrorator of sorts (possibly even tutorial guy)
+
+player is the player subclass hopefuly this class will prove to be useful for controlling all other dealers
--- src/main/java/projectName/Main.java
+++ src/main/java/projectName/Main.java
@@ -1,12 +1,70 @@
 package projectName;
-import java.util.*;
+import java.util.Scanner;
 
 public class Main 
 {
     public static void main(String[] args) {
-        System.out.println("Hi! This is an empty project./nYour code will start running from Main.java./nIt may be daunting to start a project from scratch, but try to have fun with it! :)");
-
-        // Use this space to test your code, or actually run your project
+        int i = 0;
         
+        //creates scanner
+        Scanner input = new Scanner(System.in);
+        Character Narrorator = new Character();
+    
+        // creates charater that you play as
+        Player playerOne = new Player();
+    
+    
+        //creates your pal jesse as a dealer
+        Dealer jesse = new Dealer();
+        Dealer badger = new Dealer();
+        Dealer skinnyPete = new Dealer();
+    
+    
+        //creates someone to buy items from
+        Narrorator.stateRole();
+
+        playerOne.getUserName(input);
+        // allows player to buy dealers and set ownedByPlayer to true
+        while ( i != 4){
+            System.out.println("who would you like to hire?" + "\n" + "1 : Jesse for " + jesse.getCost() +"\n" + "2 : Badger for " + badger.getCost() +"\n" + "3 : Skinny Pete for "  + skinnyPete.getCost() + "\n" + "4 : done purchasing");
+            i = input.nextInt();
+            
+            if( i == 1){
+                System.out.println("Jesser is on payroll");
+                jesse.setOwnedByPlayer(true);  
+                Double playerMon = playerOne.getMoney(); 
+                Double dealerPrice = jesse.getCost();
+                Double total = playerMon - dealerPrice;
+                playerOne.setMoney(total);
+                System.out.println("you have "+ playerOne.getMoney() + " left"); 
+            } else if (i == 2){
+                System.out.println("Badger is on payrole");
+                badger.setOwnedByPlayer(true);
+                Double playerMon = playerOne.getMoney(); 
+                Double dealerPrice = badger.getCost();
+                Double total = playerMon - dealerPrice;
+                playerOne.setMoney(total);
+                System.out.println("you have "+ playerOne.getMoney() + " left"); 
+            } else if( i == 3){
+                System.out.println("Skinny Pete is on payroll");
+                skinnyPete.setOwnedByPlayer(true);
+                Double playerMon = playerOne.getMoney(); 
+                Double dealerPrice = skinnyPete.getCost();
+                Double total = playerMon - dealerPrice;
+                playerOne.setMoney(total);
+                System.out.println("you have "+ playerOne.getMoney() + " left"); 
+            } else if (i !=4){
+                System.out.println("quit playin foo!");
+            }
+
+        }
+
+        
+
+
+
+        
+        
+
     }
 }
